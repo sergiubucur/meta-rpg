6f8f2d44f059ecafea019cda6bfeb612df4f8cee
Sergiu Bucur <sergiu.bucur@outlook.com>
2018-03-09 18:47:19 +0200

    gear layout

--- src/common/components/gear/Gear.js
+++ src/common/components/gear/Gear.js
@@ -6,6 +6,22 @@ class Gear extends Component {
 	render() {
 		return (
 			<div className="gear">
+				<div className="column1">
+					<div className="gear-cell hands"></div>
+					<div className="gear-cell main-hand"></div>
+				</div>
+
+				<div className="column2">
+					<div className="gear-cell head"></div>
+					<div className="gear-cell chest"></div>
+					<div className="gear-cell legs"></div>
+					<div className="gear-cell feet"></div>
+				</div>
+
+				<div className="column3">
+					<div className="gear-cell trinket"></div>
+					<div className="gear-cell off-hand"></div>
+				</div>
 			</div>
 		);
 	}
