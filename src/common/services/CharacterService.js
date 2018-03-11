8891590effc09f6868ea64532912444aa67d82ed
Sergiu Bucur <sergiu.bucur@outlook.com>
2018-03-11 11:59:29 +0200

    xp progression curve

--- src/common/services/CharacterService.js
+++ src/common/services/CharacterService.js
@@ -1,10 +1,49 @@
 import EventDispatcher from "simple-event-dispatcher";
 
+import XpToNextLevel, { TotalXpToLevel, MaxXp } from "./XpToNextLevel";
+
 class CharacterService {
 	events = new EventDispatcher();
 
 	level = 1;
+	xp = 0;
+	xpToNextLevel = XpToNextLevel[0];
+
 	gold = 0;
+
+	gainXp(xp) {
+		this.xp += xp;
+
+		if (this.xp > MaxXp) {
+			this.xp = MaxXp;
+		}
+
+		const level = this.calculateLevel();
+		if (this.level < level) {
+			this.level = level;
+			this.events.dispatch("levelUp");
+		}
+	}
+
+	getTotalXpToLevel() {
+		return TotalXpToLevel[this.level - 1];
+	}
+
+	calculateLevel() {
+		if (this.xp === MaxXp) {
+			return 60;
+		}
+
+		let totalXp = 0;
+		let level = 0;
+
+		while (this.xp >= totalXp) {
+			totalXp += XpToNextLevel[level];
+			level++;
+		}
+
+		return level;
+	}
 }
 
 export default new CharacterService();
